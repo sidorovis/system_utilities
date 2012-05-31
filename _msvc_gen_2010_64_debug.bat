8bd969d497c07eb140afe512e4c3aef78b8c85cb
Sergey Silaev <Sergey.Silaev@disfa.com>
2012-05-31 13:23:25 +0300

    adding generator files and tests for system_processor config examples

---
+++ _msvc_gen_2010_64_debug.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set BOOST_ROOT=d:\usr\boost_1_49_0
+
+set BUILD_TYPE=Debug
+
+set BUILD_FOLDER=_build_%BUILD_TYPE%_64
+
+if not exist %BUILD_FOLDER% (
+	mkdir %BUILD_FOLDER%
+)
+
+cd %BUILD_FOLDER%
+cmake -DVERBOSE=OFF -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DSOLUTION_NAME=system_utilities -DVERBOSE=ON -G "Visual Studio 10 Win64" ../ 
+cd ../
+
+echo "%BUILD_FOLDER%/system_utilities.sln" > _start_msvc_2010_debug.bat

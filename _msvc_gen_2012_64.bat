07842ac8b3fdbeb3b29bf7dad27034b93223ac27
ihar.marozau <igormorozov@tut.by>
2013-02-26 22:25:23 +0300

    add bat generator for VS2012

---
+++ _msvc_gen_2012_64.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set BOOST_ROOT=d:\usr\boost_1_53_0
+
+set BUILD_TYPE=Debug
+if [%1]==[Release] (
+	set BUILD_TYPE=Release
+)
+set BUILD_FOLDER=_build_%BUILD_TYPE%_64
+
+if not exist %BUILD_FOLDER% (
+	mkdir %BUILD_FOLDER%
+)
+
+cd %BUILD_FOLDER%
+cmake -DVERBOSE=OFF -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DSOLUTION_NAME=system_utilities -DVERBOSE=ON -G "Visual Studio 11 Win64" ../ 
+cd ../
+
+echo "%BUILD_FOLDER%/system_utilities.sln" > _start_msvc.bat
